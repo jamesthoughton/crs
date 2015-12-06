10b1401d864520beda191b7906c58738affe9c56
jamesthoughton <jamesthoughton@gmail.com>
2015-12-06 02:33:51 -0500

    Add more placeholder text

--- partials/news/news.php
+++ partials/news/news.php
@@ -2,7 +2,7 @@
 <div class="col-md-7">
 	<h2 class="featurette-heading" style="text-align:center;">News</h2>
 	<p class="lead" style="margin-left:10px;margin-right:10px;">
-		Cancer Research Society is a club dedicated to organizing cancer research at TJ! etc.
+		There is no news at this time...
 	</p>
 </div>
 <div class="col-md-5">

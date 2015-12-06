d73aef1fee8471c54d756689ab07fbd10fbbc8ed
jamesthoughton <jamesthoughton@gmail.com>
2015-12-05 19:12:56 -0500

    Simply partials

--- js/main.js
+++ js/main.js
@@ -5,25 +5,25 @@ $(document).ready(function() {
 	// Hook click functions
 	clicked = "a#about";
 	$('a#calendar').click(function () {
-		$('#partial').load('partials/calendar.html #content');
+		$('#partial').load('partials/calendar.html');
 		$(clicked).removeClass('engaged');
 		clicked = "a#calendar";
 		$(this).addClass('engaged');
 	});
 	$('a#about').click(function () {
-		$('#partial').load('partials/about.html #content');
+		$('#partial').load('partials/about.html');
 		$(clicked).removeClass('engaged');
 		clicked = "a#about";
 		$(this).addClass('engaged');
 	});
 	$('a#resources').click(function () {
-		$('#partial').load('partials/resources.html #content');
+		$('#partial').load('partials/resources.html');
 		$(clicked).removeClass('engaged');
 		clicked = "a#resources";
 		$(this).addClass('engaged');
 	});
 	$('a#news').click(function () { 
-		$('#partial').load('partials/news/news.php #content');
+		$('#partial').load('partials/news/news.php');
 		$(clicked).removeClass('engaged');
 		clicked = "a#news";
 		$(this).addClass('engaged');
--- partials/news/news.php
+++ partials/news/news.php
@@ -1,14 +1,3 @@
-<html>
-	<head>
-		<title>About - Cancer Research Society</title>
-		<link rel="stylesheet" href="/css/main.css" type="text/css">
-		<script src="/js/main.js"></script>
-	</head>
-	<body>
-		<div id="content" style="color:black">
-			<?php
-				echo("News partial\n");
-			?>
-		</div>
-	</body>
-</html>
+<?php
+	echo("News partial\n");
+?>

03d281c19eec0561b65f8b3e98b52b906122cb16
jamesthoughton <jamesthoughton@gmail.com>
2015-12-06 01:35:36 -0500

    Hook hash in URL

--- js/main.js
+++ js/main.js
@@ -1,31 +1,49 @@
 var clicked;
 
 $(document).ready(function() {
-
+	var url = document.URL;
+	var ind = url.indexOf('#');
+	clicked = "a#aboutl";
+	if(ind > 0) {
+		console.log(url.substring(ind));
+		switch(url.substring(ind)) {
+			case "#about":
+				clicked = "a#aboutl";
+				break;
+			case "#news":
+				clicked = "a#newsl";
+				break;
+			case "#resources":
+				clicked = "a#resourcesl";
+				break;
+			case "#calendar":
+				clicked = "a#calendarl";
+				break;
+		}
+	}
 	// Hook click functions
-	clicked = "a#about";
-	$('a#calendar').click(function () {
+	$('a#calendarl').click(function () {
 		$('#partial').load('partials/calendar.html');
 		$(clicked).removeClass('engaged');
-		clicked = "a#calendar";
+		clicked = "a#calendarl";
 		$(this).addClass('engaged');
 	});
-	$('a#about').click(function () {
+	$('a#aboutl').click(function () {
 		$('#partial').load('partials/about.html');
 		$(clicked).removeClass('engaged');
-		clicked = "a#about";
+		clicked = "a#aboutl";
 		$(this).addClass('engaged');
 	});
-	$('a#resources').click(function () {
+	$('a#resourcesl').click(function () {
 		$('#partial').load('partials/resources.html');
 		$(clicked).removeClass('engaged');
-		clicked = "a#resources";
+		clicked = "a#resourcesl";
 		$(this).addClass('engaged');
 	});
-	$('a#news').click(function () { 
+	$('a#newsl').click(function () { 
 		$('#partial').load('partials/news/news.php');
 		$(clicked).removeClass('engaged');
-		clicked = "a#news";
+		clicked = "a#newsl";
 		$(this).addClass('engaged');
 	});
 	$(clicked).click(); // Click about

72f8f084022097221d3c0d5fb36b22049c4dc8d0
jamesthoughton <jamesthoughton@gmail.com>
2015-12-05 19:04:56 -0500

    Create partials and hook click functions

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
+var clicked;
+
 $(document).ready(function() {
+
+	// Hook click functions
+	clicked = "a#about";
+	$('a#calendar').click(function () {
+		$('#partial').load('partials/calendar.html #content');
+		$(clicked).removeClass('engaged');
+		clicked = "a#calendar";
+		$(this).addClass('engaged');
+	});
+	$('a#about').click(function () {
+		$('#partial').load('partials/about.html #content');
+		$(clicked).removeClass('engaged');
+		clicked = "a#about";
+		$(this).addClass('engaged');
+	});
+	$('a#resources').click(function () {
+		$('#partial').load('partials/resources.html #content');
+		$(clicked).removeClass('engaged');
+		clicked = "a#resources";
+		$(this).addClass('engaged');
+	});
+	$('a#news').click(function () { 
+		$('#partial').load('partials/news/news.php #content');
+		$(clicked).removeClass('engaged');
+		clicked = "a#news";
+		$(this).addClass('engaged');
+	});
+	$(clicked).click(); // Click about
 });
--- partials/news/news.php
+++ partials/news/news.php
@@ -1,9 +1,14 @@
 <html>
 	<head>
-		<title>Calendar - Cancer Research Society</title>
+		<title>About - Cancer Research Society</title>
 		<link rel="stylesheet" href="/css/main.css" type="text/css">
 		<script src="/js/main.js"></script>
 	</head>
 	<body>
+		<div id="content" style="color:black">
+			<?php
+				echo("News partial\n");
+			?>
+		</div>
 	</body>
 </html>

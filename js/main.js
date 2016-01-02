1ba6f8fe6a59fc69831994db3161ec0571746e0b
jamesthoughton <jamesthoughton@gmail.com>
2016-01-02 18:03:21 -0500

    Remove size constraint for opacity, fix max-height bug

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
 		window.setTimeout(function() {
 			$('#pwrap').css('opacity',1);
 			$('#content').css('max-height',2000);
-			window.setTimeout(function(){$('#content').css('max-height',100000);$('#content').css('max-height',$('#content').height()+100);clicking=0;},500);
+			window.setTimeout(function(){$('#content').css('max-height',100000);clicking=0;},500);
 		}, 300);
 	};
 	$(clicked).addClass('engaged');
@@ -42,55 +42,59 @@ $(document).ready(function() {
 	// Hook click functions
 	$('a#aboutl').click(function () {
 		if(clicked === "a#aboutl" || clicking === 1) return;
+		$('#content').removeClass('mhtransition');
+		$('#content').css('max-height',$('#content').height()+100)
+		window.setTimeout(function(){$('#content').addClass('mhtransition');$('#content').css('max-height',0);},10);
 		clicking = 1;
 		$(clicked).removeClass('engaged');
 		clicked = "a#aboutl";
 		$(clicked).addClass('engaged');
-		$('#content').css('max-height',0);
-		if($(document).width() > 990)
-			$('#pwrap').css('opacity',0);
+		$('#pwrap').css('opacity',0);
 		window.setTimeout(function () {
 			$('#partial').load('partials/about.html',endclick());
-		}, 550);
+		}, 600);
 	});
 	$('a#resourcesl').click(function () {
 		if(clicked === "a#resourcesl" || clicking === 1) return;
+		$('#content').removeClass('mhtransition');
+		$('#content').css('max-height',$('#content').height()+100)
+		window.setTimeout(function(){$('#content').addClass('mhtransition');$('#content').css('max-height',0);},10);
 		clicking = 1;
 		$(clicked).removeClass('engaged');
 		clicked = "a#resourcesl";
 		$(clicked).addClass('engaged');
-		$('#content').css('max-height',0);
-		if($(document).width() > 990)
-			$('#pwrap').css('opacity',0);
+		$('#pwrap').css('opacity',0);
 		window.setTimeout(function () {
 			$('#partial').load('partials/resources.html',endclick());
-		}, 550);
+		}, 600);
 	});
 	$('a#newsl').click(function () { 
 		if(clicked === "a#newsl" || clicking === 1) return;
+		$('#content').removeClass('mhtransition');
+		$('#content').css('max-height',$('#content').height()+100)
+		window.setTimeout(function(){$('#content').addClass('mhtransition');$('#content').css('max-height',0);},10);
 		clicking = 1;
 		$(clicked).removeClass('engaged');
 		clicked = "a#newsl";
 		$(clicked).addClass('engaged');
-		$('#content').css('max-height',0);
-		if($(document).width() > 990)
-			$('#pwrap').css('opacity',0);
+		$('#pwrap').css('opacity',0);
 		window.setTimeout(function () {
 			$('#partial').load('partials/news.html',endclick());
-		}, 550);
+		}, 600);
 	});
 	$('a#calendarl').click(function () {
 		if(clicked === "a#calendarl" || clicking === 1) return;
+		$('#content').removeClass('mhtransition');
+		$('#content').css('max-height',$('#content').height()+100)
+		window.setTimeout(function(){$('#content').addClass('mhtransition');$('#content').css('max-height',0);},10);
 		clicking = 1;
 		$(clicked).removeClass('engaged');
 		clicked = "a#calendarl";
 		$(clicked).addClass('engaged');
-		$('#content').css('max-height',0);
-		if($(document).width() > 990)
-			$('#pwrap').css('opacity',0);
+		$('#pwrap').css('opacity',0);
 		window.setTimeout(function () {
 			$('#partial').load('partials/calendar.html',endclick());
-		}, 550);
+		}, 600);
 	});
 	$(".loading").addClass("invisible");
 });
